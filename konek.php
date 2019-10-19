0397bfc4fe419d57a0688a770adc75c6cefa63d1
mjcrvz <maryjanecruz023@gmail.com>
2019-10-19 08:09:35 +0800

    Add comments

--- konek.php
+++ konek.php
@@ -1,6 +1,7 @@
 <?php
     require_once('server.php');
 
+    //Database connection using msqli
     Class DB{
         public static function db_con(){
             $con = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
