972db5040594496952a38a3feb510f477f18e99d
mjcrvz <maryjanecruz023@gmail.com>
2019-10-19 08:02:37 +0800

    Add comments

--- server.php
+++ server.php
@@ -1,6 +1,11 @@
 <?php
+    //Set Timezone
     date_default_timezone_set("Asia/Manila");
+    
+    //define URL
     define('URL','/CRUD102/');
+
+    //Database Connection
     define('DBHOST','localhost');
     define('DBUSER','root');
     define('DBPASS','');
